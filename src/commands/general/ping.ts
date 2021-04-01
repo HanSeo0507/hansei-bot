5c8b69c1694a755e3f7e7de6fdb5fbf017800d80
HanSeo0507 <khan3618@naver.com>
2021-04-02 08:33:17 +0900

    edit: ping command run action

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,17 +1,28 @@
-import { Client, Message } from "discord.js";
+import { Message } from "discord.js";
+
 import { Command } from "src/core";
 import { IClient } from "src/@types";
 
 class Ping extends Command {
 	constructor(client) {
 		super(client);
-		this.name = "ping";
-		this.description = "get ping";
-		this.ownerOnly = true;
+		this.name = "핑";
+		this.description = "서버와 디스코드 API간의 지연속도를 안내합니다";
 	}
 
-	public async run(message: Message, arg: string[]) {
-		message.channel.send("pong");
+	public async run(message: Message, args: string[]) {
+		const embed = {
+			color: "RED",
+			title: "🏓 퐁.....",
+
+			fields: [
+				{ name: "서버 지연", value: `${Date.now() - message.createdTimestamp}ms`, inline: true },
+				{ name: "디스코드 API 지연", value: `${Math.round(this.client.ws.ping)}ms`, inline: true },
+			],
+
+			timestamp: new Date(),
+		};
+		message.channel.send({ embed });
 	}
 }
 
